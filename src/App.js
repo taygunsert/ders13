7a475facbe5333bcb56783a51fe5e575e780400b
Taygun Sert <taygunsert@outlook.com>
2023-04-09 19:26:01 +0300

    feat: Haber fetch api örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState({ hits: [] })
+  const [sorgu, sorguGuncelle] = useState("react")
+  const [ara, araGuncelle] = useState('react')
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    async function veriCek() {
+      setIsLoading(true)
+
+      const sonuc = await axios(`https://hn.algolia.com/api/v1/search?query=${ara}`)
+
+      setData(sonuc.data)
+      setIsLoading(false)
+    }
+
+    veriCek()
+
+  }, [ara])
+
   return (
-    <div>App</div>
+    <>
+      <input
+        type="text"
+        value={sorgu}
+        onChange={event => sorguGuncelle(event.target.value)}
+      />
+      <button type="button" onClick={() => araGuncelle(sorgu)}>
+        Ara
+      </button>
+
+      <h1>"{ara}" ile ilgili haberler: </h1>
+
+    {isLoading ? (
+        <div>Yükleniyor ...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )
+      }
+
+    </>
   );
 }
 
---
+++ src/Haberler.jsx
@@ -0,0 +1,1 @@
+//Bize bıraktı
