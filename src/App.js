62c2774a73a56d22a540d99b1bab0158a395c239
Taygun Sert <taygunsert@outlook.com>
2023-04-09 20:52:07 +0300

    useref basit kullanımı

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useRef, useState } from "react";
+
 function App() {
+  const [sayi, sayiGuncelle] = useState(0)
+  
+  const ozelSayi = useRef(1)
+
+  if ( sayi > 5 ) {
+    ozelSayi.current = 5
+  }
+
+  console.log(ozelSayi)
+
   return (
-    <div>App</div>
+    <div>
+      {sayi}
+      <button onClick={ ()=>{ sayiGuncelle( oncekiSayi=>oncekiSayi+1 )  } }>Arttır</button>
+    </div>
   );
 }
 
