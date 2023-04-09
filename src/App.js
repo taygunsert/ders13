e954a3cec0bbeb20711799fb7076a1d8f1086e38
Taygun Sert <taygunsert@outlook.com>
2023-04-09 20:53:13 +0300

    feat: bir form elamnına erişim için useref örneği

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { useRef } from 'react';
+
 function App() {
+  const inputRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const bilgi = inputRef.current.value
+    const regex = /\w{2,20}.*\w{2,30}/;
+    if( regex.test(bilgi) ) {
+      event.currentTarget.submit()
+    } else {
+      alert("Ad soyad en az iki kelime ve her kelime en az 2 harften oluşmalıdır.")
+    }
+    
+  }
+
   return (
-    <div>App</div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Ad Soyad:
+        <input type="text" ref={inputRef} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
-export default App;
+export default App
